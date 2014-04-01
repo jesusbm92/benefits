5a80b53b4abebb463ee163d669041b403817ba61
Felipe Crespo <felcreben@alum.us.es>
2014-04-02 00:21:34 +0200

    Repositorios, y comment servicio su lista

--- src/main/java/controllers/CommentController.java
+++ src/main/java/controllers/CommentController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.CommentService;
 import domain.Comment;
 
 @Controller
@@ -29,9 +30,9 @@ public class CommentController extends AbstractController {
 	public ModelAndView listByCustomer(@RequestParam int planId) {
 		ModelAndView result;
 
-		Collection<Comment> comments = 
-		
-		String requestURI= "comment/listByCustomer.do";
+		Collection<Comment> comments = commentService.findCommentByPlan(planId);
+
+		String requestURI = "comment/list.do";
 		result = createListModelAndView(null, comments, requestURI);
 		return result;
 	}
@@ -40,7 +41,7 @@ public class CommentController extends AbstractController {
 			Collection<Comment> comments, String requestURI) {
 		ModelAndView result;
 
-		result = new ModelAndView("comment/listByCustomer");
+		result = new ModelAndView("comment/list");
 		result.addObject("comments", comments);
 		result.addObject("requestURI", requestURI);
 
--- src/main/java/services/CommentService.java
+++ src/main/java/services/CommentService.java
@@ -1,165 +1,97 @@
 package services;
 
-import java.util.ArrayList;
 import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
-import repositories.CustomerRepository;
-import security.Authority;
-import security.LoginService;
-import security.UserAccount;
-import domain.Customer;
-import forms.CustomerForm;
+import repositories.CommentRepository;
+import domain.Comment;
 
 @Transactional
 @Service
-public class CommentService extends UserService {
+public class CommentService {
 
 	// Managed repository-----------------------
 
 	@Autowired
-	private CustomerRepository customerRepository;
+	private CommentRepository commentRepository;
 
-	// Supporting Services-----------------------
-	private Md5PasswordEncoder encoder;
+	// Supporting services -----------------
+	@Autowired
+	private PlanService planService;
 
 	// Constructors --------------------------
-
 	public CommentService() {
 		super();
 	}
 
 	// Simple CRUD methods -----------------
-	public Customer create() {
-		Customer admin = new Customer();
+	/**
+	 * Constructor por defecto de CommentService
+	 * 
+	 * @return Comment comment
+	 */
+	public Comment create() {
+		Comment comment = new Comment();
 
-		Authority auth = new Authority();
-		Collection<Authority> lAuthorty = new ArrayList<Authority>();
-		UserAccount usserA = new UserAccount();
-
-		auth.setAuthority("CUSTOMER");
-		lAuthorty.add(auth);
-		usserA.setAuthorities(lAuthorty);
-		admin.setUserAccount(usserA);
-
-		return admin;
+		return comment;
 	}
 
-	public Collection<Customer> findAll() {
-
-		return customerRepository.findAll();
+	/**
+	 * Devuelve una colección con todos los objetos de tipo Comment
+	 * 
+	 * @return Collection<Comment> comments
+	 */
+	public Collection<Comment> findAll() {
+		return commentRepository.findAll();
 	}
 
-	public Customer findOne(int customerId) {
-
-		return customerRepository.findOne(customerId);
+	/**
+	 * Devuelve una instancia de un objetos de tipo Comment En caso de no
+	 * encontrarse, devuelve null
+	 * 
+	 * @return Comment comment
+	 */
+	public Comment findOne(int commentId) {
+		return commentRepository.findOne(commentId);
 	}
 
-	public Customer findOneEdit(int customerId) {
-		Customer now = findByPrincipal();
+	/**
+	 * Persiste (guarda o crea) el objeto de tipo Comment en la base de datos a
+	 * través del repositorio CommentRepository
+	 * 
+	 * @return void
+	 */
+	public void save(Comment comment) {
+		Assert.notNull(comment);
+		// TODO Restricciones de Save
 
-		Customer customer = customerRepository.findOne(customerId);
-		return customer;
-
+		commentRepository.save(comment);
 	}
 
-	public void save(Customer customer) {
-		Assert.notNull(customer);
+	/**
+	 * Elimina el objeto de tipo Comment de la base de datos a través del
+	 * repositorio CommentRepository
+	 * 
+	 * @return void
+	 */
+	public void delete(Comment comment) {
+		Assert.notNull(comment);
+		// TODO Restricciones de Borrado
 
-		encoder = new Md5PasswordEncoder();
-
-		String oldPassword = customer.getUserAccount().getPassword();
-		String newPassword = encoder.encodePassword(oldPassword, null);
-
-		customer.getUserAccount().setPassword(newPassword);
-
-		customerRepository.save(customer);
-	}
-
-	public Customer findByPrincipal() {
-
-		Customer result;
-		UserAccount userAccount;
-		userAccount = LoginService.getPrincipal();
-		Assert.notNull(userAccount);
-		result = findByUserAccount(userAccount);
-		Assert.notNull(result);
-
-		return result;
-	}
-
-	public Customer findByUserAccount(UserAccount userAccount) {
-
-		Assert.notNull(userAccount);
-		Customer result;
-		result = customerRepository.findByUserAccountId(userAccount.getId());
-
-		return result;
-	}
-
-	// Reconstruct
-
-	public Customer reconstruct(CustomerForm customerForm) {
-		Customer customer = this.create();
-
-		customer.setEmail(customerForm.getEmail());
-		customer.setId(customerForm.getId());
-		customer.setName(customerForm.getName());
-		customer.setSurname(customer.getName());
-		customer.setBodyfat(customerForm.getBodyfat());
-		customer.setChestMeasure(customerForm.getChestMeasure());
-		customer.setNationality(customerForm.getNationality());
-		customer.setWeight(customerForm.getWeight());
-		customer.setHeight(customerForm.getHeight());
-		customer.setCity(customerForm.getCity());
-		customer.setHipMeasure(customerForm.getHipMeasure());
-		customer.setWaistlineMeasure(customerForm.getWaistlineMeasure());
-		customer.setVersion(customerForm.getVersion());
-		customer.getUserAccount().setPassword(customerForm.getPassword());
-		customer.getUserAccount().setUsername(customerForm.getUsername());
-
-		// Vemos si el nombre de usuario no está repetido
-		// Assert.isTrue(this.findActorByUsername(customerForm.getUsername()).isEmpty());
-		// Vemos que las passwords sean iguales
-		Assert.isTrue(customer.getUserAccount().getPassword()
-				.equals(customerForm.getRepeatPassword()));
-		// Vemos si ha aceptado los TOS
-		Assert.isTrue(customerForm.isTOSAccepted());
-		return customer;
-	}
-
-	public CustomerForm constructNew() {
-
-		Customer customer = this.create();
-
-		CustomerForm customerForm = new CustomerForm();
-
-		customerForm.setEmail(customer.getEmail());
-		customerForm.setId(customer.getId());
-		customerForm.setName(customer.getName());
-		customerForm.setSurname(customer.getSurname());
-		customerForm.setVersion(customer.getVersion());
-		customerForm.setBodyfat(customer.getBodyfat());
-		customerForm.setChestMeasure(customer.getChestMeasure());
-		customerForm.setNationality(customer.getNationality());
-		customerForm.setWeight(customer.getWeight());
-		customerForm.setHeight(customer.getHeight());
-		customerForm.setCity(customer.getCity());
-		customerForm.setHipMeasure(customer.getHipMeasure());
-		customerForm.setWaistlineMeasure(customer.getWaistlineMeasure());
-
-		customerForm.setPassword(customer.getUserAccount().getPassword());
-		customerForm.setUsername(customer.getUserAccount().getUsername());
-
-		return customerForm;
-
+		commentRepository.delete(comment);
 	}
 
 	// Other business methods ----------------
+
+	public Collection<Comment> findCommentByPlan(int planId) {
+		Assert.notNull(planService.findOne(planId));
+		return commentRepository.findCommentByPlan(planId);
+	}
+
+	// Assertions
 
 }
