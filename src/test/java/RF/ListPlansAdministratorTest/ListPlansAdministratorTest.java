1322c00b0bb5394005847864aa96cbee33d84e34
Felipe Crespo <felcreben@alum.us.es>
2014-04-05 22:19:47 +0200

    Test List Plan Administrator

---
+++ src/test/java/RF/ListPlansAdministratorTest/ListPlansAdministratorTest.java
@@ -0,0 +1,56 @@
+package RF.ListPlansAdministratorTest;
+
+import java.util.Collection;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.util.Assert;
+
+import services.AdministratorService;
+import services.PlanService;
+import utilities.PopulateDatabase;
+import domain.Administrator;
+import domain.Plan;
+import funcionalRequirement.GlobalTest;
+
+@ContextConfiguration(locations = { "classpath:spring/datasource.xml",
+		"classpath:spring/config/packages.xml" })
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ListPlansAdministratorTest extends GlobalTest {
+
+	@Autowired
+	AdministratorService administratorService;
+
+	@Autowired
+	PlanService planService;
+
+	@Before
+	public void setUp() {
+		PopulateDatabase.main(null);
+	}
+
+	@Test
+	public void listPlanAdministratorTest() {
+
+		authenticate("admin");
+
+		Administrator admin = administratorService.findByPrincipal();
+		Collection<Plan> plans = admin.getPlans();
+		Assert.isTrue(plans.size() == 0);
+
+	}
+
+	@Test(expected = IllegalArgumentException.class)
+	public void listPlanAdministratorCustomerLoginTest() {
+
+		authenticate("customer1");
+
+		Administrator admin = administratorService.findByPrincipal();
+		Collection<Plan> plans = admin.getPlans();
+		Assert.isTrue(plans.size() == 0);
+	}
+}
